379c1532d3162bc38f41b9a8bc02465bf7292f51
AkashMer <akashmer@gmail.com>
2023-07-30 19:38:50 +0530

    Added proper comment lines and tidied up the code a bit

--- R/createFramework.R
+++ R/createFramework.R
@@ -1,7 +1,13 @@
 createFramework <- function(..., docs = FALSE) {
+
     if(!dir.exists("data")) dir.create("data") # Creates a data subdirectory
+
     if(!dir.exists("R")) dir.create("R") # Creates an R subdirectory for code
+
     if(!dir.exists("results")) dir.create("results") # For results
+
     if(docs) dir.create("docs") # For gh-pages website
-    dir.create(...)
+
+    dir.create(...) # Creates any other desired subdirectories or files wanted
+    # by the user
 }
