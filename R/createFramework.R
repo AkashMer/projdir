aa93e2062f60e8d5c9efe88d5fa01ab24b71f02f
AkashMer <akashmer@gmail.com>
2023-07-30 19:27:16 +0530

    Added createFramework which creates a basic framework in a new project

---
+++ R/createFramework.R
@@ -0,0 +1,7 @@
+createFramework <- function(..., docs = FALSE) {
+    if(!dir.exists("data")) dir.create("data") # Creates a data subdirectory
+    if(!dir.exists("R")) dir.create("R") # Creates an R subdirectory for code
+    if(!dir.exists("results")) dir.create("results") # For results
+    if(docs) dir.create("docs") # For gh-pages website
+    dir.create(...)
+}
